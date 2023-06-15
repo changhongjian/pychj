b262586343b5fab6b9a9afc4107f264114c1d2d7
changhongjian <wozaibeihua@163.com>
2023-06-15 22:22:23 +0800

    add chj/nnet

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='chj',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description="Pylib CHJ Package",
     install_requires=[          # 添加了依赖的 package
         'opencv-python-headless',
