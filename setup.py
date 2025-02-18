b79e929b4af9012705866234864797fb5722ec10
changhongjian <wozaibeihua@163.com>
2025-02-18 22:20:13 +0800

    add some funcions in speed/split_run.py, base/file.py comm/video.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='chj',
     packages=find_packages(),
-    version='0.1.8',
+    version='0.1.9',
     description="Pylib CHJ Package",
     install_requires=[          # 添加了依赖的 package
         'opencv-python-headless',
